ced7f8a933c021b385ee701488d889ae756a4416
archisoftconsulting <github@archisoftconsulting.com>
2017-09-14 21:03:08 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,11 +14,11 @@ sudo apt-get install -y nodejs
 
 
 ## Install Yarn
-sudo apt-get install yarn    
+#sudo apt-get install yarn    
 ### OR
-#curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-#echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-#sudo apt-get update && sudo apt-get install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 ### OR
 #sudo npm install -g yarnpkg
 ### OR
