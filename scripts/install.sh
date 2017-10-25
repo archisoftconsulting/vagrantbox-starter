8b528ead55b041c2f0f7ae7585bc549e53fcd570
archisoftconsulting <github@archisoftconsulting.com>
2017-10-25 06:29:31 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,11 +2,20 @@
 #install nodejs & npm
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
-npm -v
-node -v
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
+yarn global add yo && yarn global add generator-jhipster
+
+### DONE install jhipster ###
+
+#sudo npm cache clean -f
+#sudo npm install -g n
+#sudo n stable
+#npm -v
+#node -v
 
 #instal bower.
 sudo apt-get install git-core
