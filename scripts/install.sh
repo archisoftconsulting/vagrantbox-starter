6408617db61b1982b79748c8991ad4cb5c84bce3
archisoftconsulting <github@archisoftconsulting.com>
2017-10-25 06:21:56 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 ##### Official step install jhipster #####
 #install nodejs & npm
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm cache clean -f
 sudo npm install -g n
