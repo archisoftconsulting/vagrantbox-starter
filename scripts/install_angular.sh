a97fa33c15903719d9e963021b250900f7164796
archisoftconsulting <github@archisoftconsulting.com>
2017-12-18 15:09:18 +0800

    Update install_angular.sh

--- scripts/install_angular.sh
+++ scripts/install_angular.sh
@@ -4,8 +4,8 @@ git clone http://192.168.0.204/zaki/my-admin-portal.git
 git clone http://192.168.0.204/zaki/my-buyer-portal.git
 git clone http://192.168.0.204/zaki/my-supplier-portal.git
 cd my-admin-portal
-npm install
+yarn install
 cd ../my-buyer-portal
-npm install
+yarn install
 cd ../my-supplier-portal
-npm install
+yarn install
