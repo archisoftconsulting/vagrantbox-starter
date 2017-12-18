9a4bbd00f3718487c961b549b74174c1b80d7150
archisoftconsulting <github@archisoftconsulting.com>
2017-12-18 15:00:33 +0800

    Create install_angular.sh

---
+++ scripts/install_angular.sh
@@ -0,0 +1,11 @@
+mkdir b2b
+cd b2b
+git clone http://192.168.0.204/zaki/my-admin-portal.git
+git clone http://192.168.0.204/zaki/my-buyer-portal.git
+git clone http://192.168.0.204/zaki/my-supplier-portal.git
+cd my-admin-portal
+npm install
+cd ../my-buyer-portal
+npm install
+cd ../my-supplier-portal
+npm install
